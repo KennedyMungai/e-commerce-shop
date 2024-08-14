d6c341b426551f3a8fbd660c5135e99a305031e0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-14 12:46:57 +0300

    Made some updates for the placement of the sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const Sidebar = () => {
 	return (
-		<nav className='fixed top-32 left-10 w-60 h-[70vh] overflow-y-auto shadow-sm rounded-md border p-2'>
+		<nav className='fixed top-32 left-5 w-60 h-[70vh] overflow-y-auto shadow-sm rounded-md border p-2'>
 			Sidebar
 		</nav>
 	)
