c829af45fb86c5ac99d1f34fe9d22138c86d4809
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-15 15:02:14 +0300

    Created the product form component

--- src/features/product/components/product-form.tsx
+++ src/features/product/components/product-form.tsx
@@ -1,9 +1,98 @@
+import { Button } from '@/components/ui/button'
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+import { createProduct } from '@/db/schema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 type Props = {
 	categoryId: string
 }
 
+const formSchema = createProduct.pick({
+	name: true,
+	price: true,
+	description: true,
+	categoryId: true
+})
+
+type FormValues = z.input<typeof formSchema>
+
 const ProductForm = ({ categoryId }: Props) => {
-	return <div>ProductForm</div>
+	const form = useForm<FormValues>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			name: '',
+			price: '',
+			description: '',
+			categoryId
+		}
+	})
+
+	const onSubmit = (values: FormValues) => console.log(values)
+
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)}>
+				<FormField
+					control={form.control}
+					name='name'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Name</FormLabel>
+							<FormControl>
+								<Input
+									placeholder='e.g. T-Shirt, Hoodie'
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='price'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Price</FormLabel>
+							<FormControl>
+								<Input placeholder='e.g. 1000' {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='description'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Description</FormLabel>
+							<FormControl>
+								<Textarea
+									placeholder='e.g. This is a nice product'
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type='submit' className='w-full'>
+					Create Product
+				</Button>
+			</form>
+		</Form>
+	)
 }
 
 export default ProductForm
