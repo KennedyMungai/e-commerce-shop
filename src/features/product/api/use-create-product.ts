5054760987dbbd6df196b5ca0a0c2c8b0125d358
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-15 15:11:30 +0300

    Created the hook to create a product

---
+++ src/features/product/api/use-create-product.ts
@@ -0,0 +1,27 @@
+import { client } from '@/lib/hono'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { InferRequestType, InferResponseType } from 'hono'
+import { toast } from 'sonner'
+
+type ResponseType = InferResponseType<typeof client.api.product.$post>
+type RequestType = InferRequestType<typeof client.api.product.$post>['json']
+
+export const useCreateProduct = () => {
+	const queryClient = useQueryClient()
+
+	const mutation = useMutation<ResponseType, Error, RequestType>({
+		mutationKey: ['createProduct'],
+		mutationFn: async (json) => {
+			const response = await client.api.product.$post({ json })
+
+			return await response.json()
+		},
+		onSuccess: () => {
+			toast.success('Product created successfully')
+			queryClient.invalidateQueries({ queryKey: ['products'] })
+		},
+		onError: () => toast.error('Failed to create product')
+	})
+
+	return mutation
+}
