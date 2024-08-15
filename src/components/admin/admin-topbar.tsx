a4f3a4eae19448010737c87333ccabdf645492ea
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-15 09:49:20 +0300

    Added some auth logic and a button to navuigate back to the shop

--- src/components/admin/admin-topbar.tsx
+++ src/components/admin/admin-topbar.tsx
@@ -1,3 +1,8 @@
+import { ClerkLoaded, ClerkLoading, UserButton } from '@clerk/nextjs'
+import { Loader2Icon } from 'lucide-react'
+import Link from 'next/link'
+import { Button } from '../ui/button'
+
 type Props = {
 	title: string
 }
@@ -6,6 +11,17 @@ const AdminTopbar = ({ title }: Props) => {
 	return (
 		<div className='w-full h-20 border-b shadow-sm flex items-center justify-between px-4'>
 			<p className='text-2xl font-semibold'>{title}</p>
+			<div className='flex items-center gap-x-4'>
+				<Link href={'/'}>
+					<Button variant={'outline'}>Back to Shop</Button>
+				</Link>
+				<ClerkLoading>
+					<Loader2Icon className='size-6 animate-spin' />
+				</ClerkLoading>
+				<ClerkLoaded>
+					<UserButton />
+				</ClerkLoaded>
+			</div>
 		</div>
 	)
 }
