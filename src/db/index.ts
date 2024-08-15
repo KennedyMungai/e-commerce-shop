832285c6ca1f23aa7a341b6a7606208915b7dde4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-15 12:07:20 +0300

    Deleted the path declaration

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,6 @@
 import * as schema from '@/db/schema'
-import { config } from 'dotenv'
 import { drizzle } from 'drizzle-orm/neon-http'
 import { neon } from '@neondatabase/serverless'
-
-config({ path: '.env.local' })
 
 export const sql = neon(process.env.DATABASE_URL!)
 export const db = drizzle(sql, { schema })
