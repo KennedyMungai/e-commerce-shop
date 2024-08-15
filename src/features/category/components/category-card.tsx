3f06b821202a25bf4cda319a8e05c4ac80c7b8ee
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-15 15:45:15 +0300

    Added an options button on the category card

--- src/features/category/components/category-card.tsx
+++ src/features/category/components/category-card.tsx
@@ -1,6 +1,8 @@
 'use client'
 
-import { Card } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardFooter } from '@/components/ui/card'
+import { EllipsisVerticalIcon } from 'lucide-react'
 import Link from 'next/link'
 
 type Props = {
@@ -13,10 +15,18 @@ const CategoryCard = ({ name, description, categoryId }: Props) => {
 	return (
 		<Link href={`/admin/products/${categoryId}`}>
 			<Card className='w-48 h-64 p-4'>
-				<p className='text-xl text-center pb-4'>{name}</p>
-				<p className='text-xs text-muted-foreground text-center'>
-					{description}
-				</p>
+				<CardContent className='h-auto'>
+					<p className='text-xl text-center pb-4'>{name}</p>
+					<p className='text-xs text-muted-foreground text-center'>
+						{description}
+					</p>
+				</CardContent>
+
+				<CardFooter className='h-fit'>
+					<Button className='' size='icon' variant={'outline'}>
+						<EllipsisVerticalIcon className='size-4' />
+					</Button>
+				</CardFooter>
 			</Card>
 		</Link>
 	)
