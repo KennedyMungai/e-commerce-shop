e7f6d917a94eb9cf17bfd5eca29152a0ded1c572
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-16 10:26:06 +0300

    Added the buttons to edit and delete a category

--- src/features/category/components/category-card.tsx
+++ src/features/category/components/category-card.tsx
@@ -1,9 +1,25 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardFooter } from '@/components/ui/card'
+import { Card } from '@/components/ui/card'
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger
+} from '@/components/ui/dialog'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
 import { useDeleteCategory } from '@/features/category/api/use-delete-category'
-import { TrashIcon } from 'lucide-react'
+import { MoreVerticalIcon, PencilIcon, TrashIcon } from 'lucide-react'
 import Link from 'next/link'
 
 type Props = {
@@ -16,27 +32,45 @@ const CategoryCard = ({ name, description, categoryId }: Props) => {
 	const { mutate } = useDeleteCategory(categoryId)
 
 	return (
-		<Link href={`/admin/products/${categoryId}`}>
-			<Card className='w-48 h-64 p-4'>
-				<CardContent className='h-auto'>
-					<p className='text-xl text-center pb-4'>{name}</p>
-					<p className='text-xs text-muted-foreground text-center'>
-						{description}
-					</p>
-				</CardContent>
-
-				<CardFooter className='h-fit'>
-					<Button
-						className=''
-						size='icon'
-						variant={'outline'}
-						onClick={() => mutate()}
-					>
-						<TrashIcon className='size-4' />
-					</Button>
-				</CardFooter>
-			</Card>
-		</Link>
+		<Card className='w-48 h-64 p-4'>
+			<div className='flex justify-end'>
+				<DropdownMenu>
+					<DropdownMenuTrigger>
+						<Button size='icon' variant={'outline'}>
+							<MoreVerticalIcon className='size-6' />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align='start' className='w-20'>
+						<DropdownMenuLabel>Options</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuGroup>
+							<DropdownMenuItem className='flex gap-x-2'>
+								<Button size='icon' variant={'outline'}>
+									<PencilIcon className='size-4' />
+								</Button>
+								<span className='text-xs'>Edit</span>
+							</DropdownMenuItem>
+							<DropdownMenuItem className='flex gap-x-2'>
+								<Button
+									variant={'outline'}
+									size='icon'
+									onClick={() => mutate()}
+								>
+									<TrashIcon className='size-4' />
+								</Button>
+								<span className='text-xs'>Delete</span>
+							</DropdownMenuItem>
+						</DropdownMenuGroup>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</div>
+			<Link href={`/admin/products/${categoryId}`}>
+				<p className='text-xl text-center pb-4'>{name}</p>
+				<p className='text-xs text-muted-foreground text-center'>
+					{description}
+				</p>
+			</Link>
+		</Card>
 	)
 }
 
