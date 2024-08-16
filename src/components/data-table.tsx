44dcaead9e5c0db8c95fae6d7c6c6373217c01f8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-16 15:19:09 +0300

    Added some spacing above the pagination buttons

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -97,7 +97,7 @@ export function DataTable<TData, TValue>({
 					</TableBody>
 				</Table>
 			</div>
-			<div className='flex items-center justify-end space-x-2 py-4'>
+			<div className='flex items-center justify-end space-x-2 py-4 mt-4'>
 				<Button
 					variant='outline'
 					size='sm'
