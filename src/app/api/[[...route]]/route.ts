481a03e87efe9a5a5a2950935d315c110ea3f858
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-16 12:06:24 +0300

    Created the inventory endpoint

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import category from './category'
 import product from './product'
+import inventory from './inventory'
 
 export const runtime = 'edge'
 
@@ -9,6 +10,7 @@ const app = new Hono()
 	.basePath('/api')
 	.route('/category', category)
 	.route('/product', product)
+	.route('/inventory', inventory)
 
 export const GET = handle(app)
 export const POST = handle(app)
