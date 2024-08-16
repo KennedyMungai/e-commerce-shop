4213f7b4bbd3c92dd02523b1058816d38af77ecc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-16 10:41:58 +0300

    Created a new page for the categories

--- src/app/(shop)/[categoryId]/page.tsx
+++ src/app/(shop)/[categoryId]/page.tsx
@@ -1,7 +1,29 @@
-type Props = {}
+'use client'
 
-const CategoryPage = () => {
-	return <div>CategoryPage</div>
+import { useFetchCategory } from '@/features/category/api/use-fetch-category'
+
+type Props = {
+	params: {
+		categoryId: string
+	}
+}
+
+const CategoryPage = ({ params: { categoryId } }: Props) => {
+	const {
+		data: category,
+		isPending: isCategoryPending,
+		isError: isCategoryError
+	} = useFetchCategory(categoryId)
+
+	if (isCategoryPending) {
+		return <div>Loading ...</div>
+	}
+
+	if (isCategoryError) {
+		return <div>Error</div>
+	}
+
+	return <div>{category.name}</div>
 }
 
 export default CategoryPage
