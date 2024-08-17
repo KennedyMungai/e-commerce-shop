1422faf12d6c329cafb6b3cac6c08dec3886786c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-17 11:40:03 +0300

    Updated the drizzle config file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,8 +5,11 @@ config({ path: '.env.local' })
 
 export default defineConfig({
 	schema: './src/db/schema.ts',
+	migrations: './src/db/migrations',
 	dialect: 'postgresql',
 	dbCredentials: {
 		url: process.env.DATABASE_URL!
-	}
+	},
+	strict: true,
+	verbose: true
 })
