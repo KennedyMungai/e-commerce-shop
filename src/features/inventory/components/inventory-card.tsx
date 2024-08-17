a78a7cd071e0a8372101dee9e9869bb9ad653222
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-17 12:27:46 +0300

    Adjusted the text for dark mode

--- src/features/inventory/components/inventory-card.tsx
+++ src/features/inventory/components/inventory-card.tsx
@@ -10,7 +10,7 @@ const InventoryCard = ({ href, label }: Props) => {
 	return (
 		<Link href={href}>
 			<Card className='w-48 h-64 p-4'>
-				<CardContent className='h-full w-full flex items-center justify-center uppercase font-semibold text-neutral-600 text-2xl'>
+				<CardContent className='h-full w-full flex items-center justify-center uppercase font-semibold text-neutral-600 text-2xl dark:text-neutral-300'>
 					{label}
 				</CardContent>
 			</Card>
