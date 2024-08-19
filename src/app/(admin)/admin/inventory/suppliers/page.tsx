0d0cb9e761169c4ff468d3336aea279a7ea7981a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-19 11:47:38 +0300

    Added loading and error markup to the suppliers inventory page

--- src/app/(admin)/admin/inventory/suppliers/page.tsx
+++ src/app/(admin)/admin/inventory/suppliers/page.tsx
@@ -2,6 +2,7 @@
 
 import AdminTopbar from '@/components/admin/admin-topbar'
 import { ScrollArea } from '@/components/ui/scroll-area'
+import { Skeleton } from '@/components/ui/skeleton'
 import { useFetchSuppliers } from '@/features/supplier/api/use-fetch-suppliers'
 import SupplierCard from '@/features/supplier/components/supplier-card'
 
@@ -12,12 +13,56 @@ const InventorySuppliers = () => {
 		isError: isSuppliersError
 	} = useFetchSuppliers()
 
-	if (isSuppliersError) {
-		return <div>Error</div>
+	if (isSuppliersPending) {
+		return (
+			<div>
+				<AdminTopbar title='Loading' isLoading={true} />
+				<div className='max-h-[85vh] flex items-center justify-center p-4'>
+					<ScrollArea className='h-[80vh] m-2 flex items-center justify-center'>
+						<div className='flex items-center justify-center gap-6 flex-wrap'>
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+						</div>
+					</ScrollArea>
+				</div>
+			</div>
+		)
 	}
 
-	if (isSuppliersPending) {
-		return <div>Loading...</div>
+	if (isSuppliersError) {
+		return (
+			<div>
+				<AdminTopbar title='Loading' isLoading={true} />
+				<div className='max-h-[85vh] flex items-center justify-center p-4'>
+					<ScrollArea className='h-[80vh] m-2 flex items-center justify-center'>
+						<div className='flex items-center justify-center gap-6 flex-wrap'>
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+							<Skeleton className='w-48 h-64' />
+						</div>
+					</ScrollArea>
+				</div>
+			</div>
+		)
 	}
 
 	return (
