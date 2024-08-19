188b2ae1b577e2466548b7ac80c6b8f6c0729fd3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-19 11:53:03 +0300

    Added a skeleton loader for the suppliers page

--- src/app/(admin)/admin/suppliers/page.tsx
+++ src/app/(admin)/admin/suppliers/page.tsx
@@ -4,6 +4,7 @@ import AdminTopbar from '@/components/admin/admin-topbar'
 import { DataTable } from '@/components/data-table'
 import { Button } from '@/components/ui/button'
 import { ScrollArea } from '@/components/ui/scroll-area'
+import { Skeleton } from '@/components/ui/skeleton'
 import { useFetchSuppliers } from '@/features/supplier/api/use-fetch-suppliers'
 import { supplierColumns } from '@/features/supplier/components/supplier-columns'
 import SuppliersSheet from '@/features/supplier/components/suppliers-sheet'
@@ -18,11 +19,39 @@ const SuppliersPage = () => {
 	} = useFetchSuppliers()
 
 	if (isSuppliersLoading) {
-		return <p>Loading...</p>
+		return (
+			<div>
+				<AdminTopbar title={'Loading'} isLoading={true} />
+				<div className='p-4 flex flex-col gap-y-4'>
+					<div className='w-full flex items-center justify-between'>
+						<Button variant={'outline'} onClick={onOpen} disabled>
+							Add Supplier
+						</Button>
+					</div>
+					<ScrollArea className='h-[80vh] p-4'>
+						<Skeleton className='w-[80vw] h-[70vh]' />
+					</ScrollArea>
+				</div>
+			</div>
+		)
 	}
 
 	if (isSuppliersError) {
-		return <p>Error</p>
+		return (
+			<div>
+				<AdminTopbar title={'Loading'} isLoading={true} />
+				<div className='p-4 flex flex-col gap-y-4'>
+					<div className='w-full flex items-center justify-between'>
+						<Button variant={'outline'} onClick={onOpen} disabled>
+							Add Supplier
+						</Button>
+					</div>
+					<ScrollArea className='h-[80vh] p-4'>
+						<Skeleton className='w-[80vw] h-[70vh]' />
+					</ScrollArea>
+				</div>
+			</div>
+		)
 	}
 
 	return (
