7eff99d795c3258fd5b4da3c42739cea94b72bf5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-19 11:39:19 +0300

    Implemented skeleton loading for the sidebar component

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -4,6 +4,7 @@ import { useFetchCategories } from '@/features/category/api/use-fetch-categories
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { Button } from '@/components/ui/button'
+import { Skeleton } from './ui/skeleton'
 
 const Sidebar = () => {
 	const pathname = usePathname()
@@ -17,11 +18,39 @@ const Sidebar = () => {
 	} = useFetchCategories()
 
 	if (isCategoriesPending) {
-		return <div>Loading ...</div>
+		return (
+			<nav className='fixed top-32 left-5 w-60 h-[70vh] overflow-y-auto shadow-sm rounded-md border p-2'>
+				<div className='flex flex-col gap-y-2 py-2 overflow-y-auto'>
+					<Skeleton className='h-10 w-full' />
+					<Skeleton className='h-10 w-full' />
+					<Skeleton className='h-10 w-full' />
+					<Skeleton className='h-10 w-full' />
+					<Skeleton className='h-10 w-full' />
+					<Skeleton className='h-10 w-full' />
+					<Skeleton className='h-10 w-full' />
+					<Skeleton className='h-10 w-full' />
+					<Skeleton className='h-10 w-full' />
+				</div>
+			</nav>
+		)
 	}
 
 	if (isCategoriesError) {
-		return <div>Error</div>
+		return (
+			<nav className='fixed top-32 left-5 w-60 h-[70vh] overflow-y-auto shadow-sm rounded-md border p-2'>
+				<div className='flex flex-col gap-y-2 py-2 overflow-y-auto'>
+					<Skeleton className='h-10 w-full' />
+					<Skeleton className='h-10 w-full' />
+					<Skeleton className='h-10 w-full' />
+					<Skeleton className='h-10 w-full' />
+					<Skeleton className='h-10 w-full' />
+					<Skeleton className='h-10 w-full' />
+					<Skeleton className='h-10 w-full' />
+					<Skeleton className='h-10 w-full' />
+					<Skeleton className='h-10 w-full' />
+				</div>
+			</nav>
+		)
 	}
 
 	return (
@@ -30,7 +59,7 @@ const Sidebar = () => {
 				Categories
 			</p>
 			<div className='flex flex-col gap-y-2 py-2 overflow-y-auto'>
-				{categories.map((category) => (
+				{categories?.map((category) => (
 					<Link key={category.id} href={`/${category.id}`}>
 						<Button
 							variant={
