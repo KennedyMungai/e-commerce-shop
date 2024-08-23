673f53f65f1a8f7c824334416d1983aabeeb7906
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-23 09:55:52 +0300

    Added loading and error states for the product details page

--- src/app/(shop)/categories/[categoryId]/productDetail/[productId]/page.tsx
+++ src/app/(shop)/categories/[categoryId]/productDetail/[productId]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
 import { useFetchProduct } from '@/features/product/api/use-fetch-product'
 import { HeartIcon, ShoppingCartIcon } from 'lucide-react'
 import Image from 'next/image'
@@ -19,11 +20,49 @@ const ProductPage = ({ params: { productId } }: Props) => {
 	} = useFetchProduct(productId)
 
 	if (isProductPending) {
-		return <div>Loading...</div>
+		return (
+			<div className='h-full w-full grid grid-cols-2'>
+				<div className='h-full w-full flex items-center justify-center bg-white rounded-md'>
+					<div className='absolute bottom-4 flex gap-2 items-center justify-center'>
+						<Skeleton className='w-36 h-10' />
+					</div>
+				</div>
+				<div className='h-full w-full flex flex-col items-center justify-center p-4'>
+					<p className='text-3xl font-semibold animate-pulse'>
+						Loading Product Name ...
+					</p>
+					<p className='text-neutral-600 dark:text-neutral-300 animate-pulse'>
+						Loading Product Description ...
+					</p>
+					<div className='absolute bottom-4'>
+						<Skeleton className='w-36 h-10 animate-bounce' />
+					</div>
+				</div>
+			</div>
+		)
 	}
 
 	if (isProductError) {
-		return <div>Error</div>
+		return (
+			<div className='h-full w-full grid grid-cols-2'>
+				<div className='h-full w-full flex items-center justify-center bg-white rounded-md'>
+					<div className='absolute bottom-4 flex gap-2 items-center justify-center'>
+						<Skeleton className='w-36 h-10' />
+					</div>
+				</div>
+				<div className='h-full w-full flex flex-col items-center justify-center p-4'>
+					<p className='text-3xl font-semibold animate-pulse'>
+						Loading Product Name ...
+					</p>
+					<p className='text-neutral-600 dark:text-neutral-300 animate-pulse'>
+						Loading Product Description ...
+					</p>
+					<div className='absolute bottom-4'>
+						<Skeleton className='w-36 h-10 animate-bounce' />
+					</div>
+				</div>
+			</div>
+		)
 	}
 
 	return (
