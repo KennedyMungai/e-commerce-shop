7286ef22769cb327e23aca5e4ee140d3fde4e8e4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-23 10:53:43 +0300

    Added the items to the cart component

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,18 +1,32 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
 import {
 	Popover,
 	PopoverContent,
 	PopoverTrigger
 } from '@/components/ui/popover'
-import { Button } from '@/components/ui/button'
+import { useFetchCartItems } from '@/features/cart/api/use-fetch-cart-items'
 import { ShoppingCartIcon } from 'lucide-react'
-import { Badge } from './ui/badge'
+import { Fragment } from 'react'
+import { Separator } from '@/components/ui/separator'
 
 type Props = {}
 
 const Cart = () => {
-	// const {} = useFetchCartItems()
+	const {
+		data: cartItems,
+		isPending: isCartItemsPending,
+		isError: isCartItemsError
+	} = useFetchCartItems()
+
+	if (isCartItemsPending) {
+		return <div>Loading...</div>
+	}
+
+	if (isCartItemsError) {
+		return <div>Error</div>
+	}
 
 	return (
 		<Popover>
@@ -22,7 +36,15 @@ const Cart = () => {
 				</Button>
 			</PopoverTrigger>
 			<PopoverContent className='w-56 h-96 overflow-y-auto p-2'>
-				lorem500
+				{cartItems?.map((item) => (
+					<Fragment key={item.id}>
+						<div>
+							{item.product.name} -{' '}
+							{Number(item.product.price) * item.quantity!}
+						</div>
+						<Separator />
+					</Fragment>
+				))}
 			</PopoverContent>
 		</Popover>
 	)
